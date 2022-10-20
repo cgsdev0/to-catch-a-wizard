9d0cec8d8df8ae0222591b60c543fa582b059dcc
Sarah Schulte <sarah.schulte@samsara.com>
2022-10-20 15:59:57 -0700

    SPRING CLEANING

---
+++ scripts/TorchDoor.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+export var torches: Array = []
+
+var actual_torches = []
+func open():
+	$CollisionPolygon2D.disabled = true
+	$TorchDoor.texture = preload("res://sprites/props/door_open.png")
+	
+func close():
+	$CollisionPolygon2D.disabled = false
+	$TorchDoor.texture = preload("res://sprites/props/torch_door.png")
+
+func _ready():
+	for torch in torches:
+		actual_torches.append(get_node(torch))
+		
+func _physics_process(delta):
+	for torch in actual_torches:
+		if !torch.lit:
+			close()
+			return
+	open()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
